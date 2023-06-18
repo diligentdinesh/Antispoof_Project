af8d9a4db71d179de4ac3a9a29c0c0ce087e5c27
bekojuniranjan <bekojuniranjan@gmail.com>
2023-06-18 12:27:04 +0545

    Feature extraction code latest version

--- feature_extraction/ALDP.py
+++ feature_extraction/ALDP.py
@@ -31,8 +31,7 @@ def aldp_threshold_neg(x, threshold):
         return 1
     else:
         return 0
-def calculate_alpha_parameter(frame):
-    center_value = frame[4]
+def calculate_alpha_parameter(frame, center_value):
     frame = np.array(frame)
     return np.sum(frame-center_value) / center_value
 
@@ -69,16 +68,17 @@ def calculate_aldp_value(mask_value, alpha):
     
     return pos_code, neg_code
 
-def ELDP(sound_signal, frame_length=9):
+def ALDP(sound_signal, frame_length=9):
     total_frame = len(sound_signal) // frame_length
 
     aldp_list_pos = list()
     aldp_list_neg = list()
     masks = [get_krisch_mask(i) for i in range(8)]
+    center_value = max(sound_signal)
     for i in range(total_frame):
         frame = sound_signal[i * frame_length : (i + 1) * frame_length]
         mask_value = calculate_mask_value(frame, masks)
-        alpha = calculate_alpha_parameter(frame)
+        alpha = calculate_alpha_parameter(frame, center_value)
         pos_code, neg_code = calculate_aldp_value(mask_value=mask_value, alpha=alpha)
 
         aldp_pos = int("".join([str(i) for i in pos_code]), 2)
@@ -86,25 +86,25 @@ def ELDP(sound_signal, frame_length=9):
         aldp_list_pos.append(aldp_pos)
         aldp_list_neg.append(aldp_neg)
     
-    plt.figure()
-    plt.hist(aldp_list_pos, bins=10, density=True)
-    plt.title("ALDP pos")
-    plt.show()
+    # plt.figure()
+    # plt.hist(aldp_list_pos, bins=10, density=True)
+    # plt.title("ALDP pos")
+    # plt.show()
 
-    plt.figure()
-    plt.hist(aldp_list_neg, bins=10, density=True)
-    plt.title("ALDP neg")
-    plt.show()
+    # plt.figure()
+    # plt.hist(aldp_list_neg, bins=10, density=True)
+    # plt.title("ALDP neg")
+    # plt.show()
 
     ALDP_pos, _ = np.histogram(aldp_list_pos, bins=10, density=True)
     ALDP_neg, _ = np.histogram(aldp_list_neg, bins=10, density=True)
-    ELDP = np.concatenate((ALDP_pos, ALDP_neg))
-    return ELDP
+    ALDP = np.concatenate((ALDP_pos, ALDP_neg))
+    return ALDP
 
 
 if __name__ == "__main__":
     filename = librosa.example("nutcracker")
     y, sr = librosa.load(filename)
     write("sound.wav", sr, y)
-    eltp_features = ELDP(y)
+    eltp_features = ALDP(y)
     print(eltp_features)
