79bcec85647883968355413fa280d354fa7c7443
bekojuniranjan <bekojuniranjan@gmail.com>
2023-06-23 03:09:55 +0000

    Feature extraction

--- feature_extraction/feature_extraction.py
+++ feature_extraction/feature_extraction.py
@@ -1,17 +1,17 @@
 import librosa
 import os
 import matplotlib.pyplot as plt 
-from Feature_Model import ELTP, ALDP, LFCC
+from feature_model import ELTP, ALDP, LFCC, LFCC_pipeline
 from tqdm import tqdm
 import pandas as pd
 
-train_dir = "/media/niranjan/Volume_1/Voice_Spoofing/datasets/LA/ASVspoof2019_LA_train/flac"
-eval_dir = "/media/niranjan/Volume_1/Voice_Spoofing/datasets/LA/ASVspoof2019_LA_eval/flac"
-dev_dir = "/media/niranjan/Volume_1/Voice_Spoofing/datasets/LA/ASVspoof2019_LA_dev/flac"
+train_dir = "/home/user/Antispoof_Project/LA/ASVspoof2019_LA_train/flac"
+eval_dir = "/home/user/Antispoof_Project/LA/ASVspoof2019_LA_eval/flac"
+dev_dir = "/home/user/Antispoof_Project/LA/ASVspoof2019_LA_dev/flac"
 
-train_features_from_datasets = "/media/niranjan/Volume_1/Voice_Spoofing/datasets/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt"
-eval_features_from_datasets  = "/media/niranjan/Volume_1/Voice_Spoofing/datasets/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt"
-dev_features_from_datasets   = "/media/niranjan/Volume_1/Voice_Spoofing/datasets/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.dev.trl.txt"
+train_features_from_datasets = "/home/user/Antispoof_Project/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt"
+eval_features_from_datasets  = "/home/user/Antispoof_Project/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt"
+dev_features_from_datasets   = "/home/user/Antispoof_Project/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.dev.trl.txt"
 
 def get_track_list(directory):
     return os.listdir(directory)
@@ -21,14 +21,14 @@ def read_track_file(directory, filename):
     y, sr = librosa.load(file_name, sr=16000)
     return y
 
-track_list = get_track_list(dev_dir)
+track_list = get_track_list(eval_dir)
 
 
 features_of_all_track = []
 # i = 1
 for track in tqdm(track_list):
-    y = read_track_file(dev_dir, track)
-    eltp = LFCC.lfcc(y)
+    y = read_track_file(eval_dir, track)
+    eltp = LFCC_pipeline.lfcc(y)
     features = [('FEATURE_'+str(i+1), feature) for i,feature in enumerate(eltp)]
     features =dict(features)
     features['AUDIO_FILE_NAME'] = track.split('.')[0]
@@ -40,7 +40,7 @@ for track in tqdm(track_list):
 features_df = pd.DataFrame(features_of_all_track)
 
 
-train_features =pd.read_csv(dev_features_from_datasets, header=None, delimiter=r"\s+")
+train_features =pd.read_csv(eval_features_from_datasets, header=None, delimiter=r"\s+")
 train_features.columns = ['SPEAKER_ID', 'AUDIO_FILE_NAME', 'SYSTEM_ID', '-', 'KEY']
 result = train_features.merge(features_df, on='AUDIO_FILE_NAME', how='inner')
-result.to_csv('../extracted_features/lfcc_dev.csv')
+result.to_csv('../extracted_features/lfcc_features_new/lfcc_eval.csv')
